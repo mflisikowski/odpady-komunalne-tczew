c4fe3855cdf2016cfc7e10ee6b369d27372a1c89
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-04 22:44:21 +0200

    fix: test 3

--- app/api/waste/mixed/route.ts
+++ app/api/waste/mixed/route.ts
@@ -5,30 +5,26 @@ import { NextResponse } from "next/server";
 import { defaultStreet } from "@/config";
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
 
-    const street = searchParams.get("street") || defaultStreet.id;
-    const date = searchParams.get("date") || null;
-    const days = searchParams.get("days") || "64";
+  const street = searchParams.get("street") || defaultStreet.id;
+  const date = searchParams.get("date") || null;
+  const days = searchParams.get("days") || "64";
 
-    let url = new URL("https://odpadykomunalne.tczew.pl/?p=1-harmonogram&t-1");
+  let url = new URL("https://odpadykomunalne.tczew.pl/?p=1-harmonogram&t-1");
 
-    url.searchParams.append("s", street);
-    if (date) url.searchParams.append("date", date);
-    if (days) url.searchParams.append("d", days);
+  url.searchParams.append("s", street);
+  if (date) url.searchParams.append("date", date);
+  if (days) url.searchParams.append("d", days);
 
-    const mixed = await fetchData(url.toString(), parseMixedDetails);
-    const metadata = await fetchData(url.toString(), parseMetadata);
+  const mixed = await fetchData(url.toString(), parseMixedDetails);
+  const metadata = await fetchData(url.toString(), parseMetadata);
 
-    return NextResponse.json({
-      original_data_url: url.toString(),
-      metadata,
-      waste: {
-        mixed,
-      },
-    });
-  } catch (error) {
-    return NextResponse.error();
-  }
+  return NextResponse.json({
+    original_data_url: url.toString(),
+    metadata,
+    waste: {
+      mixed,
+    },
+  });
 }
