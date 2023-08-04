43668d8095aee1727e3703ecaec9cbb925e8247e
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-04 22:31:26 +0200

    fix: test one

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,4 @@
-import ChooseForm from "@/components/choose-form";
-
-// async function fetchStreets() {
-//   const response = await fetch(`http://localhost:3000/api/waste/streets`, {
-//     next: {
-//       revalidate: 60 * 60 * 24, // 24 hours
-//     }
-//   });
-//   const { streets } = await response.json();
-
-//   if (!response.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch streets data from API');
-//   }
-
-//   return streets || [];
-// }
-
 export default async function Page() {
-  // const streets = await fetchStreets();
-
   return (
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
       <header className="space-y-4">
@@ -31,7 +11,7 @@ export default async function Page() {
         </p>
       </header>
 
-      {/* <ChooseForm streets={streets} /> */}
+      TEST
     </div>
   )
 }
