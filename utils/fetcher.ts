4161a58f285973e285f6b401c3db20faa6b5f258
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-06 12:23:27 +0200

    feat: add new fetcher utility for data retrieval
    Introduced a new utility function, fetcher, to handle data fetching with default options and error handling. The function constructs the API URL based on protocol and hostname headers and merges default fetch options with the provided ones.

---
+++ utils/fetcher.ts
@@ -0,0 +1,51 @@
+import { headers } from "next/headers";
+
+async function fetcher(endpointName: string, endpointOptions?: RequestInit) {
+  const defaultHostname = "localhost:3000";
+  const defaultProtocol = "http";
+
+  const protocol = headers().get("protocol") || defaultProtocol;
+  const hostname = headers().get("host") || defaultHostname;
+
+  const defaultOptions = {
+    next: {
+      revalidate: 60 * 60 * 1, // 1 hour
+    },
+  };
+
+  const options = {
+    ...defaultOptions,
+    ...endpointOptions,
+  };
+
+  const url = `${protocol}://${hostname}/${endpointName}`;
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      return {
+        data: null,
+        error: {
+          message: `API responded with error: ${response.statusText}  `,
+        },
+      };
+    }
+
+    const data = await response.json();
+
+    return {
+      data,
+      error: null,
+    };
+  } catch (err) {
+    return {
+      data: null,
+      error: {
+        message: `API responded with error: ${JSON.stringify(err) || err}`,
+      },
+    };
+  }
+}
+
+export default fetcher;
