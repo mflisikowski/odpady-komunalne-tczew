6957bcb4433ff7123f2bd61176defd7b06534d30
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-06 20:25:26 +0200

    refactor: streamline layout structure in page components
    - Introduced a new `Loading` component to display loading state.
    - Simplified the DOM structure in `mixed/page.tsx` and `selective/page.tsx` by removing unnecessary wrapping div elements.

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl text-center space-y-24">
+                <p>Trwa ładowanie danych ulic</p>
+            </div>
+        </div>
+    )
+}
--- app/mixed/page.tsx
+++ app/mixed/page.tsx
@@ -19,47 +19,45 @@ export default async function Page() {
   }
 
   return (
-    <div className="">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center space-y-24">
-          <header className="space-y-4">
-            {!error && data?.metadata?.title && (
-              <h1 className="text-3xl leading-9 font-extrabold text-slate-900 tracking-tight sm:text-4xl sm:leading-10">
-                {data?.metadata?.title}
-              </h1>
-            )}
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl text-center space-y-24">
+        <header className="space-y-4">
+          {!error && data?.metadata?.title && (
+            <h1 className="text-3xl leading-9 font-extrabold text-slate-900 tracking-tight sm:text-4xl sm:leading-10">
+              {data?.metadata?.title}
+            </h1>
+          )}
 
-            {!error && data?.metadata?.subtitle && (
-              <p className="text-xl leading-7 text-slate-900">
-                {data?.metadata?.subtitle}
-              </p>
-            )}
+          {!error && data?.metadata?.subtitle && (
+            <p className="text-xl leading-7 text-slate-900">
+              {data?.metadata?.subtitle}
+            </p>
+          )}
 
-            {!error && data?.original_data_url && (
-              <p className="text-sm leading-7 text-slate-900">
-                Projekt korzysta z publicznie dostępnych danych {" "}
+          {!error && data?.original_data_url && (
+            <p className="text-sm leading-7 text-slate-900">
+              Projekt korzysta z publicznie dostępnych danych {" "}
 
-                <a href={data?.original_data_url} className="underline underline-offset-auto ">
-                  www.odpadykomunalne.tczew.pl
-                </a>
-              </p>
-            )}
+              <a href={data?.original_data_url} className="underline underline-offset-auto ">
+                www.odpadykomunalne.tczew.pl
+              </a>
+            </p>
+          )}
 
-            {!error && data?.waste?.mixed && (
-              <div className="flex justify-center">
-                <SwitchData />
-              </div>
-            )}
-          </header>
+          {!error && data?.waste?.mixed && (
+            <div className="flex justify-center">
+              <SwitchData />
+            </div>
+          )}
+        </header>
 
-          <Suspense fallback={<p>Trwa ładowanie danych...</p>}>
-            {!error && data?.waste?.mixed && (
-              <div className="max-h-96">
-                <StackedLists wasteSchedule={mixed} />
-              </div>
-            )}
-          </Suspense>
-        </div>
+        <Suspense fallback={<p>Trwa ładowanie danych...</p>}>
+          {!error && data?.waste?.mixed && (
+            <div className="max-h-96">
+              <StackedLists wasteSchedule={mixed} />
+            </div>
+          )}
+        </Suspense>
       </div>
     </div>
   )
