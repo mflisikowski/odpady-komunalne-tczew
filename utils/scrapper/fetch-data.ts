dff83d9e3785fb2b119493ac4c19eb241882ffda
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-09 18:00:45 +0200

    refactor: simplify and document fetchData function in scrapper utility
    Changes made:
    - Introduced documentation comments to describe the `fetchData` function's purpose, parameters, and return type.
    - Simplified the fetch request by removing the cache option since the default behavior is typically adequate.
    - Replaced the `let` declaration with `const` for cheerio's `$` since it's not being reassigned.

--- utils/scrapper/fetch-data.ts
+++ utils/scrapper/fetch-data.ts
@@ -1,13 +1,16 @@
 import { load } from "cheerio";
 
-export const fetchData = async (url: string, parser: any) => {
+/**
+ * Fetches data from the provided URL and parses it using the given parser function.
+ * @param url - The URL to fetch data from.
+ * @param parser - The function to parse the fetched data.
+ * @returns The parsed data or an empty array if there was an error.
+ */
+export const fetchData = async (url: string, parser: any): Promise<any[]> => {
   try {
-    const response = await fetch(url, {
-      cache: "no-store",
-    });
-
+    const response = await fetch(url);
     const html = await response.text();
-    let $ = load(html);
+    const $ = load(html);
     return parser($);
   } catch (error) {
     console.error(`Error fetching data from ${url}:`, error);
