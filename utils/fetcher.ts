e6472f03e13fd524cfbaaa6281d94d59c6e79fd2
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-09 17:42:15 +0200

    refactor: enhance clarity and documentation in fetcher utility
    Changes made:
    - Added comprehensive documentation comments for the `fetcher` function to explain its purpose, parameters, and return type.
    - Enhanced readability by adding comments to sections of the code to describe their functionality.
    - Improved the error message's clarity by removing the trailing whitespace.

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -7,6 +7,12 @@ interface ExtendedRequestInit extends RequestInit {
   };
 }
 
+/**
+ * Makes a fetch request to the specified endpoint with the given options.
+ * @param endpointName - The name of the endpoint URL.
+ * @param endpointOptions - Additional options for the fetch request.
+ * @returns An object containing the response data or an error message.
+ */
 async function fetcher(
   endpointName: string,
   endpointOptions: ExtendedRequestInit = {}
@@ -14,6 +20,7 @@ async function fetcher(
   const defaultHostname = "localhost:3000";
   const defaultProtocol = "http";
 
+  // Get the protocol and hostname from the headers or use default values
   const protocol = headers().get("protocol") || defaultProtocol;
   const hostname = headers().get("host") || defaultHostname;
 
@@ -23,32 +30,39 @@ async function fetcher(
     },
   };
 
+  // Merge the default options with the endpoint options
   const options: ExtendedRequestInit = {
     ...defaultOptions,
     ...endpointOptions,
   };
 
+  // Construct the URL for the fetch request
   const url = `${protocol}://${hostname}/${endpointName}`;
 
   try {
+    // Make the fetch request
     const response = await fetch(url, options);
 
     if (!response.ok) {
+      // Return an error object if the response is not successful
       return {
         data: null,
         error: {
-          message: `API responded with error: ${response.statusText}  `,
+          message: `API responded with error: ${response.statusText}`,
         },
       };
     }
 
+    // Parse the response body as JSON
     const data = await response.json();
 
+    // Return the response data
     return {
       data,
       error: null,
     };
   } catch (err) {
+    // Return an error object if an error occurs during the fetch request
     return {
       data: null,
       error: {
