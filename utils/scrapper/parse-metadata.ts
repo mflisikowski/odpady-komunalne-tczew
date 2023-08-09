4fba7b1ab5eca92b1bdd56444d765242dac665cb
Mateusz Flisikowski <contact@mflisikowski.dev>
2023-08-09 19:21:06 +0200

    refactor: simplify metadata parsing logic in parse-metadata utility
    Changes made:
    - Added a descriptive docstring to the `parseMetadata` function for clarity.
    - Removed array destructuring and direct mapping in favor of distinct variables for each metadata item. This results in more readable code.
    - Cleaned up the return object to directly use parsed variables.

--- utils/scrapper/parse-metadata.ts
+++ utils/scrapper/parse-metadata.ts
@@ -1,11 +1,14 @@
+/**
+ * Parses the metadata from a webpage.
+ * @param $ - The library object used for parsing HTML.
+ * @returns An object containing the extracted subtitle and title.
+ */
 export const parseMetadata = ($: any) => {
-  const [$subtitle, $title] = [
-    "#content div.row-form-top > div:nth-child(2) > div:nth-child(1)",
-    "#content div.row-form-top > div.page-header-title > h2",
-  ].map((element) => $(element).text().trim());
+  const subtitle = $("#content div.row-form-top > div:nth-child(2) > div:nth-child(1)").text().trim();
+  const title = $("#content div.row-form-top > div.page-header-title > h2").text().trim();
 
   return {
-    subtitle: $subtitle.trim(),
-    title: $title,
+    subtitle,
+    title,
   };
 };
